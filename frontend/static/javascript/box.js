e9aac49844f7e9986886487ffd51436da4d062c0
Elsa <zeyugao@outlook.com>
2020-01-16 10:30:43 +0800

    Add display rate compared to last week

--- frontend/static/javascript/box.js
+++ frontend/static/javascript/box.js
@@ -56,6 +56,9 @@ var config = {
                     display: true,
                     position: 'left',
                     id: 'sale',
+                    ticks: {
+                        suggestedMin: 0.00
+                    }
                 },
                 {
                     type: 'linear',
@@ -63,27 +66,10 @@ var config = {
                     position: 'right',
                     id: 'rate',
                     ticks: {
-                        suggestedMax: 1.00,
                         suggestedMin: 0.00
                     }
                 },
-            ],/*
-            sale: {
-                display: true,
-                scaleLabel: {
-                    display: true,
-                    labelString: 'Sale'
-                },
-                position: 'left',
-            },
-            rate: {
-                type: 'linear',
-                display: true,
-                position: 'right',
-                gridLines: {
-                    drawOnChartArea: false,
-                }
-            }*/
+            ],
         }
     }
 };
@@ -96,6 +82,27 @@ function close_modal() {
     setTimeout(function () {
         $('#loading_modal').modal('hide')
     }, 500)
+}
+
+let expected_movie_name = [
+    "アナと雪の女王２",
+    "スター・ウォーズ"
+];
+
+let day_offset = 0;
+
+let display_rate = true;
+
+function onclick_checkbox_display_rate() {
+    if ($("#display_rate").is(':checked')) {
+        display_rate = true;
+        window.chart.config.data = JSON.parse(JSON.stringify(default_data));
+        update_chart(expected_movie_name, day_offset);
+    } else {
+        display_rate = false;
+        window.chart.config.data = JSON.parse(JSON.stringify(default_data));
+        update_chart(expected_movie_name, day_offset);
+    }
 }
 
 var colorNames = Object.keys(window.chartColors);
@@ -126,9 +133,6 @@ function update_chart(names, day_offset = 0) {
                 if (movie_data[item] === undefined) {
                     movie_data[item] = [];
                 }
-                if (movie_data[item + 'rate'] === undefined) {
-                    movie_data[item + 'rate'] = [];
-                }
                 new_datasets[item] = {
                     label: item,
                     backgroundColor: new_color,
@@ -138,17 +142,22 @@ function update_chart(names, day_offset = 0) {
                     lineTension: 0,
                     yAxisID: 'sale'
                 };
-                color_count++;
-                new_color = window.chartColors[colorNames[color_count % colorNames.length]];
-                new_datasets[item + 'rate'] = {
-                    label: item + ' Rate',
-                    backgroundColor: new_color,
-                    borderColor: new_color,
-                    data: movie_data[item + 'rate'],
-                    fill: false,
-                    //lineTension: 0,
-                    yAxisID: 'rate',
-                };
+                if (display_rate) {
+                    new_color = window.chartColors[colorNames[color_count % colorNames.length]];
+                    if (movie_data[item + 'rate'] === undefined) {
+                        movie_data[item + 'rate'] = [];
+                    }
+                    new_datasets[item + 'rate'] = {
+                        label: item + ' Rate',
+                        backgroundColor: new_color,
+                        borderColor: new_color,
+                        data: movie_data[item + 'rate'],
+                        fill: false,
+                        //lineTension: 0,
+                        borderDash: [5, 5],
+                        yAxisID: 'rate',
+                    };
+                }
                 color_count++;
             });
             var box = resp.data[0];
@@ -158,7 +167,8 @@ function update_chart(names, day_offset = 0) {
                     new_exists_time.push(key);
                     box[key].forEach(function (movie) {
                         movie_data[movie.name].push({x: moment(key), y: movie.sale});
-                        movie_data[movie.name + 'rate'].push({x: moment(key), y: movie.rate});
+                        if (display_rate)
+                            movie_data[movie.name + 'rate'].push({x: moment(key), y: movie.rate});
                     });
                 }
             }
@@ -175,7 +185,6 @@ function update_chart(names, day_offset = 0) {
                     config.data.datasets.push(new_datasets[key]);
                 }
             }
-            console.log(config.data.datasets);
             close_modal();
             window.chart.update();
         })
@@ -188,7 +197,7 @@ function update_chart(names, day_offset = 0) {
 }
 
 function update_chart_wrapper() {
-    update_chart(["アナと雪の女王２"]);//, "スター・ウォーズ スカイウ"]);
+    update_chart(expected_movie_name, day_offset);
 }
 
 window.onload = function () {
